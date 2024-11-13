e9ab482cbf650497a85cadf6cc1d472892d84d1c
Athiratv98 <athira.narayanan998@gmail.com>
2024-11-13 10:05:45 +0530

    DailyTask added

---
+++ PythonTraining_DailyTask/SQL_Learning/SQL_CRUD.py
@@ -0,0 +1,47 @@
+import sqlite3
+connection=sqlite3.connect(r"C:\Users\Administrator\Desktop\PythonTraining\PythonTraining_DailyTask\SQL_Learning\Chinook_Sqlite.sqlite")
+cursor=connection.cursor()
+print(cursor)
+#create operations
+# new_customer=(60,'Rajiv','RR','UST','Rajiv@gmail.com','IND')
+# cursor.execute(
+#     """INSERT into Customer(CustomerId,Firstname,Lastname,Company,Email,Country) values(?,?,?,?,?,?)
+#     """,new_customer
+# )
+
+# connection.commit()
+# print("new cusyomer added")
+#read the datas that are just wrote in to database
+cursor.execute("select * from 'Customer' where CustomerID=60")
+data=cursor.fetchall()
+for row in data:
+    print(row)
+desc=cursor.description
+cols=[col[0] for col in desc]
+print(cols)
+
+#updation
+customer_id=60
+customer_mail="rajivn456@ust.com"
+cursor.execute(
+    """
+    update Customer 
+    set Email=?,Company=?
+    where CustomerId=?
+    """,(customer_mail,"TCS",customer_id)
+)
+connection.commit()
+print("Customer Updated")
+cursor.execute("select * from 'Customer' where CustomerID=60")
+data=cursor.fetchall()
+for row in data:
+    print(row)
+desc=cursor.description
+cols=[col[0] for col in desc]
+print(cols)
+
+# deletion
+cursor.execute("Delete from Customer where CustomerID=60")
+
+connection.commit()
+print("ID 60 isdeleted")
--- PythonTraining_DailyTask/SQL_Learning/SqlTest_Demo.py
+++ PythonTraining_DailyTask/SQL_Learning/SqlTest_Demo.py
@@ -2,7 +2,15 @@ import sqlite3
 connection=sqlite3.connect(r"C:\Users\Administrator\Desktop\PythonTraining\PythonTraining_DailyTask\SQL_Learning\Chinook_Sqlite.sqlite")
 cursor=connection.cursor()
 print(cursor)
-cursor.execute("select name from sqlite_master where type='table';")
-tables=cursor.fetchall()
-for table in tables:
-    print(table)
+#cursor.execute("select name from sqlite_master where type='table';")
+# tables=cursor.fetchall()
+# for table in tables:
+#     print(table)
+
+cursor.execute("select * from 'Album' limit 10")
+data=cursor.fetchall()
+for row in data:
+    print(row)
+desc=cursor.description
+cols=[col[0] for col in desc]
+print(cols)
